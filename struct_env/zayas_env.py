05b9ace366e26f0b4cf1c261609c3cee79c1024d
Pablo G <pgmorato@uliege.be>
2022-11-19 16:35:18 +0100

    Cleaning up the environment

--- struct_env/zayas_env.py
+++ struct_env/zayas_env.py
@@ -7,29 +7,20 @@ class Struct:
         if config is None:
             config = {"n_comp": 22,
                       "discount_reward": 0.95,
-                      "k_comp": None,
-                      "env_correlation": False,
                       "campaign_cost": False}
         assert "n_comp" in config and \
                "discount_reward" in config and \
-               "k_comp" in config and \
-               "env_correlation" in config and \
                "campaign_cost" in config, \
             "Missing env config"
 
         self.n_comp = config["n_comp"]
         self.discount_reward = config["discount_reward"]
-        self.k_comp = self.n_comp - 1 if config["k_comp"] is None \
-            else config["k_comp"]
-        self.env_correlation = config["env_correlation"]
         self.campaign_cost = config["campaign_cost"]
         self.time = 0
         self.ep_length = 30  # Horizon length
         self.n_st_comp = 30  # Crack states (fatigue hotspot damage states)
-        self.n_st_hyperp = 80 if self.env_correlation else None
-        # Gaussian hyperparemeter states
         self.n_obs = 2
-        # Total number of observations (crack detected / crack not detected)
+        # Total number of observations per hotspot (crack detected / crack not detected)
         self.actions_per_agent = 3
 
         # Uncorrelated obs = 30 per agent + 1 timestep
@@ -39,55 +30,27 @@ class Struct:
         self.obs_total_single = None  # Todo: check used in gym env
 
         ### Loading the underlying POMDP model ###
-        if not self.env_correlation:
-            drmodel = np.load('pomdp_models/zayas_input.npz')
-        else:
-            drmodel = np.load('pomdp_models/Dr3031_H08.npz')
+        drmodel = np.load('pomdp_models/zayas_input.npz')
 
         # (ncomp components, nstcomp crack states)
         self.belief0 = np.zeros((self.n_comp, self.n_st_comp))
 
         self.indZayas = drmodel['indZayas']
         self.relSysc = drmodel['relSysc']
+        self.n_elem = 13
 
-        if not self.env_correlation:
-            self.belief0[:, :] = drmodel['belief0'][0, 0, :, 0]
+        self.belief0[:, :] = drmodel['belief0'][0, 0, :, 0]
 
-            # (3 actions, 10 components, 31 det rates, 30 cracks, 30 cracks)
-            self.P = drmodel['P'][:, 0, :, :, :]
+        # (3 actions, 10 components, 31 det rates, 30 cracks, 30 cracks)
+        self.P = drmodel['P'][:, 0, :, :, :]
 
-            # (3 actions, 10 components, 30 cracks, 2 observations)
-            self.O = drmodel['O'][:, 0, :, :]
-            
-            self.belief0c = None
-            self.b0cR = None
-            self.alpha0 = None
+        # (3 actions, 10 components, 30 cracks, 2 observations)
+        self.O = drmodel['O'][:, 0, :, :]
 
-        else:
-            self.belief0[:, :] = drmodel['belief0']
-            # (3 actions, 31 det rates, 30 cracks, 30 cracks)
-            self.P = drmodel['P']
-
-            # (3 actions, 30 cracks, 2 observations)
-            self.O = drmodel['O']
-
-            self.belief0c = \
-                np.zeros((self.n_comp, self.n_st_hyperp, self.n_st_comp))
-            self.belief0c[:, :, :] = drmodel['belief0c']
-
-            # conditional beliefs associated with a repair action
-            # (80 hyperparameter states, 30 crack states)
-            self.b0cR = drmodel['b0cR']
-
-            # hyperparameter marginal states
-            self.alpha0 = drmodel['alpha0']
-
-        self.agent_list = ["agent_" + str(i) for i in range(self.n_comp)]
+        self.agent_list = ["agent_" + str(i) for i in range(self.n_comp)] # (must be 13 elements and agents here)
 
         self.time_step = 0
         self.beliefs = self.belief0
-        self.beliefsc = self.belief0c
-        self.alphas = self.alpha0
         self.d_rate = np.zeros((self.n_comp, 1), dtype=int)
         self.observations = None
 
@@ -101,14 +64,11 @@ class Struct:
         # Choose the agent's belief
         self.time_step = 0
         self.beliefs = self.belief0
-        self.beliefsc = self.belief0c
-        self.alphas = self.alpha0
         self.d_rate = np.zeros((self.n_comp, 1), dtype=int)
         self.observations = {}
         for i in range(self.n_comp):
             self.observations[self.agent_list[i]] = np.concatenate(
                 (self.beliefs[i], [self.time_step / self.ep_length]))
-
 
         return self.observations
 
@@ -117,15 +77,11 @@ class Struct:
         for i in range(self.n_comp):
             action_[i] = action[self.agent_list[i]]
 
-        if not self.env_correlation:
-            observation_, belief_prime, drate_prime = \
-                self.belief_update_uncorrelated(self.beliefs, action_,
-                                                self.d_rate)
+        observation_, belief_prime, drate_prime = \
+            self.belief_update_uncorrelated(self.beliefs, action_,
+                                            self.d_rate)
 
-        else:
-            observation_, belief_prime, drate_prime, bc_prime, alpha_prime = \
-                self.belief_update_correlated(self.beliefsc, action_,
-                                              self.d_rate, self.alphas)
+
 
         reward_ = self.immediate_cost(self.beliefs, action_, belief_prime,
                                       self.d_rate)
@@ -141,10 +97,6 @@ class Struct:
         for i in range(self.n_comp):
             self.observations[self.agent_list[i]] = np.concatenate(
                 (belief_prime[i], [self.time_step / self.ep_length]))
-
-        if self.env_correlation:
-            self.beliefsc = bc_prime
-            self.alphas = alpha_prime
 
         self.beliefs = belief_prime
         self.d_rate = drate_prime
@@ -244,46 +196,3 @@ class Struct:
                 # been accounted in the env transition
                 drate_prime[i, 0] = 0
         return ob, b_prime, drate_prime
-
-    def belief_update_correlated(self, bc, a, drate, alpha):
-        """Bayesian belief update based on previous belief,
-         current observation, and action taken"""
-
-        b_prime = np.zeros((self.n_comp, self.n_st_comp))
-        bc_prime = np.zeros((self.n_comp, self.n_st_hyperp, self.n_st_comp))
-        drate_prime = np.zeros((self.n_comp, 1), dtype=int)
-        alpha_prime = np.zeros((self.n_st_hyperp))
-        alpha_prime = alpha.copy()
-        ob = np.zeros(self.n_comp)
-        for i in range(self.n_comp):
-            p1 = bc[i, :, :].dot(
-                self.P[a[i], drate[i, 0]])  # environment transition
-            bc_prime[i, :, :] = p1
-            drate_prime[i, 0] = drate[i, 0] + 1
-
-            if a[i] == 1:
-                Obs0 = np.sum(alpha[:].dot(p1) * self.O[a[i], :, 0])
-                Obs1 = 1 - Obs0
-                if Obs1 < 1e-5:
-                    ob[i] = 0
-                else:
-                    ob_dist = np.array([Obs0, Obs1])
-                    ob[i] = np.random.choice(range(0, self.n_obs), size=None,
-                                             replace=True, p=ob_dist)
-                pInsp = p1 * self.O[a[i], :, int(ob[i])]  # belief update
-                likAlpha = np.sum(pInsp, axis=1)  # likelihood insp alpha
-                normBel = np.tile(likAlpha, self.n_st_comp).reshape(
-                    self.n_st_hyperp, self.n_st_comp,
-                    order='F')  # normalization constant
-                bc_prime[i, :, :] = pInsp / normBel
-                alpha_curr = likAlpha * alpha_prime
-                alpha_prime = alpha_curr / np.sum(alpha_curr)
-
-            if a[i] == 2:
-                bc_prime[i, :, :] = self.b0cR
-                drate_prime[i, 0] = 0
-
-        for i in range(self.n_comp):
-            b_prime[i, :] = alpha_prime.dot(
-                bc_prime[i, :, :])  # Belief (marginalize out alpha)
-        return ob, b_prime, drate_prime, bc_prime, alpha_prime
