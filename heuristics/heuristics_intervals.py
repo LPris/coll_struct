b7c81899f02d3e59fca2275d30e4468f00b7c90e
Pablo G <pgmorato@uliege.be>
2022-11-27 17:03:09 +0100

    heuristics pf_sys

--- heuristics/heuristics_intervals.py
+++ heuristics/heuristics_intervals.py
@@ -34,7 +34,7 @@ class Heuristics():
     def search(self, eval_size):
         insp_interval = np.arange(1, self.struct_env.ep_length)
         comp_inspection = np.arange(1, self.struct_env.n_elem+1)
-        pf_brace_rep_fix = 0.01
+        pf_sys_rep = 0.001
         heur = np.meshgrid(insp_interval, comp_inspection)
         insp_list = heur[0].reshape(-1)
         comp_list = heur[1].reshape(-1)
@@ -44,7 +44,7 @@ class Heuristics():
         for ind in range(len(insp_list)):
             return_heur = 0
             for _ in range(eval_size):
-                return_heur += self.episode(insp_list[ind], comp_list[ind], pf_brace_rep_fix)
+                return_heur += self.episode(insp_list[ind], comp_list[ind], pf_sys_rep)
             return_heur /= eval_size
             ret_total.append(return_heur)
             if return_heur > ret_opt:
@@ -62,17 +62,17 @@ class Heuristics():
         np.savez('heuristics/Results/heuristics_'+ self.date_record, ret_total = ret_total, opt_heur = self.opt_heur)
         return self.opt_heur
 
-    def eval(self, eval_size, insp_int, comp_insp, pf_brace_rep):
+    def eval(self, eval_size, insp_int, comp_insp, pf_sys_rep):
         self.return_heur = 0
         for ep in range(eval_size):
-            self.return_heur += self.episode(insp_int, comp_insp, pf_brace_rep)
+            self.return_heur += self.episode(insp_int, comp_insp, pf_sys_rep)
             disp_cost = self.return_heur/(ep+1)
             if ep%500==0:
                 print('Reward:', disp_cost)
         self.return_heur /= eval_size
         return self.return_heur
     
-    def episode(self, insp_int, comp_insp, pf_brace_rep):
+    def episode(self, insp_int, comp_insp, pf_sys_rep):
         rew_total_ = 0
         done_ = False
         insp_obs = 2
@@ -95,9 +95,12 @@ class Heuristics():
                     for index in index_repair:
                         agent_ind = self.struct_env.comp_agent[index]
                         action_[self.struct_env.agent_list[agent_ind]] = 2
-            for i in range(self.struct_env.n_elem):
-                if self.struct_env.pf_brace[i] > pf_brace_rep:
-                    action_[self.struct_env.agent_list[i]] = 2
+            Pf_fat = self.struct_env.beliefs[:, -1].copy()
+            pf_sys_eval = self.struct_env.pf_sys(Pf_fat, self.struct_env.pf_brace)
+            if pf_sys_eval > pf_sys_rep:
+                for i in range(self.struct_env.n_elem):
+                    if self.struct_env.pf_brace[i] > 0.001:
+                        action_[self.struct_env.agent_list[i]] = 2
             [_, rew_, done_, insp_obs] = self.struct_env.step(action_)
             rew_total_ += rew_['agent_0']
         return rew_total_
